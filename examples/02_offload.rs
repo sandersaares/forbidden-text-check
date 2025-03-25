8c69a013d40f10380ed566dacac7f8ea9826848f
Sander Saares <sander@saares.eu>
2025-03-25 22:12:12 +0200

    pre-warm

--- examples/02_offload.rs
+++ examples/02_offload.rs
@@ -5,6 +5,9 @@ use tokio::spawn;
 
 #[tokio::main]
 async fn main() {
+    // Pre-warm the data set so the first request does not get penalized.
+    _ = ILLEGAL_NUMBERS.len();
+    
     spawn(async move {
         let app = Router::new().route("/check", post(check_number));
 
